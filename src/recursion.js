0c04702fa86be83b29c205f8d8751dccca35729b
Christopher Pruijsen <christopher.pruijsen@gmail.com>
2016-06-07 23:36:13 -0700

    PowerOfTwo

--- src/recursion.js
+++ src/recursion.js
@@ -142,6 +142,13 @@ var exponent = function(base, exp, result, posOrNeg) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  if(n === 2 || n === 1){
+    return true;
+  } else if(n<2){
+    return false;
+  } else {
+    return powerOfTwo(n/2);
+  }
 };
 
 // 9. Write a function that accepts a string a reverses it.
